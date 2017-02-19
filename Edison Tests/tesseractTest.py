d68a6f165d3d4717b0b185316d33fe60b6c448e4
ribsthakkar <rishabh.thakkar@gmail.com>
2017-02-19 09:33:36 -0600

    Tesseract testing with various image modification tools.
    Added all of the code used to convert image into a scan and tested with
    tesseract.

--- Edison Tests/tesseractTest.py
+++ Edison Tests/tesseractTest.py
@@ -1,8 +1,15 @@
 from PIL import Image
 import pytesseract
+import cv2
 
-image_file = 'unnamed.jpg'
-im = Image.open(image_file) #opening image file
+image = cv2.imread('unnamed4.jpg')
+orig = image.copy()
+orig = cv2.fastNlMeansDenoisingColored(orig,None,10,10,7,21)
+gray = cv2.cvtColor(orig, cv2.COLOR_BGR2GRAY)
+gray = cv2.fastNlMeansDenoising(gray,None,10,7,21)
+cv2.imwrite("dst.jpg",gray)
+tess_file = 'dst.jpg'
+im = Image.open(tess_file) #opening image file
 im = im.save('test.tif')
 im = Image.open('test.tif') #change image format
 im.load()
---
+++ Image Modification Tests/ocrTest.py
@@ -0,0 +1,62 @@
+from PIL import Image
+import sys
+
+import pyocr
+import pyocr.builders
+
+tools = pyocr.get_available_tools()
+if len(tools) == 0:
+    print("No OCR tool found")
+    sys.exit(1)
+# The tools are returned in the recommended order of usage
+tool = tools[0]
+print("Will use tool '%s'" % (tool.get_name()))
+# Ex: Will use tool 'libtesseract'
+
+langs = tool.get_available_languages()
+print("Available languages: %s" % ", ".join(langs))
+lang = langs[0]
+print("Will use lang '%s'" % (lang))
+# Ex: Will use lang 'fra'
+# Note that languages are NOT sorted in any way. Please refer
+# to the system locale settings for the default language
+# to use.
+txt = tool.image_to_string(
+    Image.open('unnamed.jpg'),
+    lang=lang,
+    builder=pyocr.builders.TextBuilder()
+)
+# txt is a Python string
+print txt
+"""word_boxes = tool.image_to_string(
+    Image.open('test.png'),
+    lang="eng",
+    builder=pyocr.builders.WordBoxBuilder()
+)
+# list of box objects. For each box object:
+#   box.content is the word in the box
+#   box.position is its position on the page (in pixels)
+#
+# Beware that some OCR tools (Tesseract for instance)
+# may return empty boxes
+
+line_and_word_boxes = tool.image_to_string(
+    Image.open('test.png'), lang="fra",
+    builder=pyocr.builders.LineBoxBuilder()
+)
+# list of line objects. For each line object:
+#   line.word_boxes is a list of word boxes (the individual words in the line)
+#   line.content is the whole text of the line
+#   line.position is the position of the whole line on the page (in pixels)
+#
+# Beware that some OCR tools (Tesseract for instance)
+# may return empty boxes
+
+# Digits - Only Tesseract (not 'libtesseract' yet !)
+digits = tool.image_to_string(
+    Image.open('test-digits.png'),
+    lang=lang,
+    builder=pyocr.tesseract.DigitBuilder()
+)
+# digits is a python string
+"""
